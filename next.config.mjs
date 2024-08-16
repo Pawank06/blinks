5efef2ca3e64aa06eb9727d4ff46dcd11d9ace8d
PawanK06 <pawan06kumar2003@gmail.com>
2024-08-17 01:43:06 +0530

    fix cors error

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
       return [
         {
-          source: "/api/actions/transfer-sol",
+          source: "/api/transfer-sol",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
